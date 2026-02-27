9984279c66a7d124cf4e56d185d855652af53dc2
kesav <kesav5470@gmail.com>
2026-02-27 22:43:23 +0530

    [Kesav Mangalathil] added usecase6 Palindrome Checker

--- PalindromeCheckerApp.java
+++ PalindromeCheckerApp.java
@@ -1,19 +1,41 @@
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+import java.util.Stack;
+
 public class PalindromeCheckerApp {
     public static void  main(String arg[]){
-        String word = "madam";
+        Scanner sc = new Scanner(System.in);
 
-        
-        String reversed = "";
+        System.out.print("Enter a string: ");
+        String input = sc.nextLine();
 
-        for (int i = word.length() - 1; i >= 0; i--) {
-            reversed = reversed + word.charAt(i);
+        input = input.toLowerCase(); 
+
+        Stack<Character> stack = new Stack<>();
+        Queue<Character> queue = new LinkedList<>();
+
+        for (int i = 0; i < input.length(); i++) {
+            char ch = input.charAt(i);
+            stack.push(ch);
+            queue.add(ch);  
+
+        boolean isPalindrome = true;
+
+
+        while (!queue.isEmpty()) {
+            if (queue.remove() != stack.pop()) {
+                isPalindrome = false;
+                break;
+            }
         }
 
-        if (word.equals(reversed)) {
-            System.out.println(word + " is a Palindrome.");
-        } else {
-            System.out.println(word + " is NOT a Palindrome.");
-        }
+        if (isPalindrome)
+            System.out.println("It is a Palindrome.");
+        else
+            System.out.println("Not a Palindrome.");
+
+        sc.close();
     }
-    
+}
 }
